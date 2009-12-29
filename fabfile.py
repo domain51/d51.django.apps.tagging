1cde4208a6fcaa06c507d743a82a50aaeae29847
Travis Swicegood <development@domain51.com>
2009-12-29 16:30:17 -0600

    remove all the buildout cruft

--- fabfile.py
+++
@@ -1,9 +0,0 @@
-from fabric.api import *
-import os
-
-def test():
-    if os.path.exists("./project-for-testing"):
-        local("mv ./project-for-testing ./project")
-    local("bin/django test tagging")
-    local("mv ./project ./project-for-testing")
-
